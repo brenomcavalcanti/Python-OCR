1f9343d8f35126cb517e194202d6c0e8a0bfdefd
Breno Cavalcanti <breno.maranhao.cavalcanti@gmail.com>
2025-12-17 05:33:58 -0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -6,9 +6,10 @@ class Debug:
 
     def bugReport(item, location, image):
 
-        print('[BugReport]: Este item (',item,') não pôde ser trasnformado no tipo desejável de variável.\nO item foi adicionado na posição ',location,' da lista principal e a imagem que o gerou foi salva em save/debug/debugImage'+str(Debug.bugCounter)+'.jpg.\n')
+        print('[BugReport]: Este item (',item,') não pôde ser trasnformado no tipo desejável de variável.\nA imagem que o gerou foi salva em save/debug/debugImage'+str(Debug.bugCounter)+'.jpg.\n')
 
         with open('../save/debug/debugList.txt', 'a') as debugList:
             debugList.write('Bug de número: '+str(Debug.bugCounter)+'.\nLocal: '+str(location)+'.\nItem: '+str(item)+'.\nHorário: '+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+'.\n\n')
         Debug.bugCounter += 1
+
         image.save('../save/debug/debugImage'+str(Debug.bugCounter)+datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")+'.jpg')
